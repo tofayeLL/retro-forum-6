e7a282daeb9425d062999ef200eda5ef948d0eb8
tofayeLL <ahmedtofayel132@gmail.com>
2024-03-03 13:08:27 +0600

    finished latest post to do dynamic

--- script.js
+++ script.js
@@ -4,6 +4,8 @@
 const postContainer = document.getElementById('post-container');
 const displayTitle = document.getElementById('display-title-view');
 const DisplayMarkRead = document.getElementById('mark-read');
+const latestPostContainer = document.getElementById('latest-post');
+
 
 
 
@@ -11,6 +13,9 @@ const DisplayMarkRead = document.getElementById('mark-read');
 
 let markRead = 0;
 
+
+
+// Load All Posts
 const loadAllPosts = async()=>{
     const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/posts');
     const data = await res.json();
@@ -23,7 +28,7 @@ const loadAllPosts = async()=>{
         else{
             active = `<img class="absolute top-2 right-3" src="images/icons/Status (1).png" alt="">`
         }
-        console.log(post);
+        // console.log(post);
         const div = document.createElement('div');
         div.innerHTML = `
         <!-- card 1 -->
@@ -76,6 +81,7 @@ const loadAllPosts = async()=>{
 }
 
 
+// display title and view
 const displayTitleView = (title,view)=>{ 
     markRead = markRead + 1;
     DisplayMarkRead.innerText = markRead;
@@ -92,9 +98,50 @@ const displayTitleView = (title,view)=>{
 }
 
 
+// load Latest post
+
+const loadLatestPost = async()=>{
+    const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/latest-posts');
+    const data = await res.json();
+    data.forEach((post)=>{
+        console.log(post);
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <!-- card 1 -->
+        <div class="bg-base-100 shadow-xl p-5 space-y-4 rounded-lg">
+            <figure><img class="rounded-sm" src="${post.cover_image}" alt="Shoes" /></figure>
+            <div class="space-y-4 ">
+                <p class="flex gap-2"><img src="images/icons/Frame.png" alt="">
+                     <span>${post.author.posted_date || 'No Publish Date'}</span>
+                </p>
+              <h2 class="text-lg font-semibold ">${post.title}</h2>
+              <p class="text-sm ">${post.description.slice(0,90)}</p>
+              <div class="flex gap-5 items-center ">
+                <div  class="w-[15%] ">
+                    <img src="${post.profile_image}" alt="" class="w-[100%] rounded-full">
+                </div>
+                <div>
+                    <p class="text-base font-semibold">${post.author.name}</p>
+                    <p class="text-sm">${post.author.designation || 'Unknown'}</p>
+                </div>
+              </div>
+              
+            </div>
+          </div>
+        
+        
+        `
+        latestPostContainer.appendChild(div);
+        
+    })
+
+}
+
+
 
 
 
 
 
 loadAllPosts();
+loadLatestPost();
